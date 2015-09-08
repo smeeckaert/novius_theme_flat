e1a2f7d322f633109d2ac0ae0ecb803acf70dd85
Martin Smeeckaert <smeeckaert@novius.fr>
2015-09-08 17:15:23 +0200

    use jjg

---
+++ config/apps/novius_theme_manager/themes.config.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+    'flat' => array(
+        'name' => __('Flat'),
+        'css'  => array(
+            'minified' => array(
+                "monmin.css",
+            ),
+            'raw'      => array(
+                "monraw.css",
+            ),
+        ),
+    ),
+);
---
+++ config/metadata.config.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * NOVIUS OS - Web OS for digital communication
+ *
+ * @copyright  2011 Novius
+ * @license    GNU Affero General Public License v3 or (at your option) any later version
+ *             http://www.gnu.org/licenses/agpl-3.0.html
+ * @link       http://www.novius-os.org
+ */
+
+return array(
+    'name'       => 'Flat Design Theme',
+    'version'    => '5.0.1 (Elche)',
+    'provider'   => array(
+        'name' => 'Smeeckaert Martin',
+    ),
+    'extends'    => array('novius_theme_manager'),
+    'namespace'  => 'Nos\Theme\Flat',
+    'permission' => array(),
+    'launchers'  => array(),
+    'enhancers'  => array(),
+    'icons'      => array(
+        16 => 'static/apps/novius_theme_flat/img/icon/16.png',
+        32 => 'static/apps/novius_theme_flat/img/icon/32.png',
+        64 => 'static/apps/novius_theme_flat/img/icon/64.png',
+    ),
+);
